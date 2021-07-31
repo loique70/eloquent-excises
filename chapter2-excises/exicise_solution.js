ccd14e58421ef78fa666bafd70350eab0138aa74
fogouang <lfogouang39@gmail.com>
2021-07-31 20:04:46 +0100

    second

--- chapter2-excises/exicise_solution.js
+++ chapter2-excises/exicise_solution.js
@@ -1,12 +1,41 @@
 //Looping Triangles
 
-let myResult ='';
-let callNumber = 7;
-for(let i =1;i<=callNumber;i++){
-    for(let j =1; j<=i;j++){
-        myResult += '*';
+// let myResult ='';
+// let callNumber = 7;
+// for(let i =1;i<=callNumber;i++){
+//     for(let j =1; j<=i;j++){
+//         myResult += '#';
+//     }
+//     myResult += '\n'
+// }
+// //Output myResult
+// console.log(myResult)
+
+//FizzBuzz
+let tabVal = [];
+let tabVal1 = [];
+let tabVal2 = [];
+let a =''
+let totalNumber = 100;
+for(let i=1;i<=totalNumber;i++){
+    if(i % 3 === 0){
+        console.log('Fizz');
+        tabVal.push(i)
     }
-    myResult += '\n'
+    else if( i % 5 === 0){
+        console.log('Buzz');
+        tabVal1.push(i)
+     }
+     else{
+        console.log(i)
+     }
+     if(i%3 === 0 && i%5 === 0){
+         console.log('FizzBuzz')
+         tabVal2.push(i)
+     }
+   
 }
-//Output myResult
-console.log(myResult)
+// The number d
+console.log(tabVal)
+console.log(tabVal1)
+console.log(tabVal2)
