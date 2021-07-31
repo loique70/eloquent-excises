11dd080ed053a555c9d0dcd8bf064ee902944c84
fogouang <74138682+loique70@users.noreply.github.com>
2021-07-31 17:12:25 +0100

    Update exicise_solution.js

--- chapter2-excises/exicise_solution.js
+++ chapter2-excises/exicise_solution.js
@@ -4,7 +4,7 @@ let myResult ='';
 let callNumber = 7;
 for(let i =1;i<=callNumber;i++){
     for(let j =1; j<=i;j++){
-        myResult += '*';
+        myResult += '#';
     }
     myResult += '\n'
 }
