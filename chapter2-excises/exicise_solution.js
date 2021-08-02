6f383a3ffd1d2588fc256876b11383b3bdf94b41
fogouang <fogouangloic10@gmail.com>
2021-08-02 11:47:16 +0100

    new commit

--- chapter2-excises/exicise_solution.js
+++ chapter2-excises/exicise_solution.js
@@ -1,15 +1,15 @@
 //Looping Triangles
 
-// let myResult ='';
-// let callNumber = 7;
-// for(let i =1;i<=callNumber;i++){
-//     for(let j =1; j<=i;j++){
-//         myResult += '#';
-//     }
-//     myResult += '\n'
-// }
-// //Output myResult
-// console.log(myResult)
+let myResult ='';
+let callNumber = 7;
+for(let i =1;i<=callNumber;i++){
+    for(let j =1; j<=i;j++){
+        myResult += '#';
+    }
+    myResult += '\n'
+}
+//Output myResult
+console.log(myResult)
 
 //FizzBuzz
 let tabVal = [];
